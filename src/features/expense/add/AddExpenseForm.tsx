500c53bd8639ae0f0a46c61e1a4b0bd89f789994
Vadim Garbuzov <v1.garbuzow@yandex.ru>
2026-04-22 18:13:54 +0300

    Добавление useForm

--- src/features/expense/add/AddExpenseForm.tsx
+++ src/features/expense/add/AddExpenseForm.tsx
@@ -1,16 +1,23 @@
 import styles from "./AddExpenseForm.module.scss";
 import Field from "@/shared/ui/field";
 import Button from "@/shared/ui/button";
-import { type Expense, useExpensesApi } from "@/entities/expense/model";
+import { useExpensesApi } from "@/entities/expense/model";
 import { v4 as uuid } from "uuid";
 import { type FieldValues, type SubmitHandler, useForm } from "react-hook-form";
+import { useEffect } from "react";
+
+type FormValues = {
+  ownerName: string;
+  amount: number;
+};
 
 const AddExpenseForm = () => {
   const {
     register,
     handleSubmit,
+    setFocus,
     formState: { errors },
-  } = useForm();
+  } = useForm<FormValues>({ mode: "onBlur" });
   const { addExpense } = useExpensesApi();
 
   const onSubmit: SubmitHandler<FieldValues> = ({ ownerName, amount }) => {
@@ -22,13 +29,18 @@ const AddExpenseForm = () => {
     addExpense(newExpense);
   };
 
+  useEffect(() => {
+    setFocus("ownerName");
+  }, [setFocus]);
+
   return (
     <form className={styles.newExpenseForm} onSubmit={handleSubmit(onSubmit)}>
       <Field
-        id="name"
+        id="ownerName"
         label="Имя"
         autoComplete="off"
-        {...register("name", {
+        error={errors?.ownerName?.message}
+        {...register("ownerName", {
           required: "Имя обязательно",
           minLength: {
             value: 2,
@@ -42,13 +54,14 @@ const AddExpenseForm = () => {
         label="Сумма"
         type="number"
         autoComplete="off"
+        error={errors?.amount?.message}
         {...register("amount", {
           required: "Сумма обязательна",
           min: {
             value: 1,
             message: "Минимум 1",
           },
-          valueAsNumber: true, // Важно для числовых полей
+          valueAsNumber: true,
         })}
       />
       <Button className={styles.addExpenseButton} type="submit">
--- src/shared/ui/field/Field.tsx
+++ src/shared/ui/field/Field.tsx
@@ -4,15 +4,22 @@ import { type FC, type InputHTMLAttributes } from "react";
 type FieldProps = {
   id: string;
   label?: string;
+  error?: string | undefined;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-const Field: FC<FieldProps> = ({ id, label, ...inputProps }) => {
+const Field: FC<FieldProps> = ({ id, label, error, ...inputProps }) => {
   return (
     <div className={styles.field}>
-      <input className={styles.input} id={id} placeholder="" {...inputProps} />
+      <input
+        className={`${styles.input} ${error && styles.inputError}`}
+        id={id}
+        placeholder=""
+        {...inputProps}
+      />
       <label className={`${styles.label}`} htmlFor={id}>
         {label}
       </label>
+      {error && <span className={styles.error}>{error}</span>}
     </div>
   );
 };
